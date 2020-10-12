56496f3c6883c91a76cb4a17ecf96884b687f2b9
priyanka kalamegam <kpriyankaoff@gmail.com>
2020-10-12 13:20:43 +0530

    Updates

---
+++ precache-manifest.026bf660b7541254179212ecd3d444cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a156f9ce6f486f17c3742e587c398e56",
+    "url": "/gallery/index.html"
+  },
+  {
+    "revision": "d838a6d6fe978d6ca6aa",
+    "url": "/gallery/static/css/main.1fe9766d.chunk.css"
+  },
+  {
+    "revision": "241417568b1e25892607",
+    "url": "/gallery/static/js/2.be6a46c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gallery/static/js/2.be6a46c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d838a6d6fe978d6ca6aa",
+    "url": "/gallery/static/js/main.b41377df.chunk.js"
+  },
+  {
+    "revision": "2f4930a35620dfd5166b",
+    "url": "/gallery/static/js/runtime-main.22064c4d.js"
+  }
+]);
